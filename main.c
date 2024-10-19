7b8752574bf8bb362cdf509acc9947a905dc7b29
mathews <mathewsvinoy@gmail.com>
2024-10-19 01:09:54 -0400

    window

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-void main(){
-    printf("hello World");
+
+static void activate (GtkApplication *app, gpointer  user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title(GTK_WINDOW (window), "Text Editor");
+  gtk_window_set_default_size(GTK_WINDOW(window), 1110, 650);
+
+  gtk_window_present(GTK_WINDOW (window));
+}
+
+int main (int    argc,char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
