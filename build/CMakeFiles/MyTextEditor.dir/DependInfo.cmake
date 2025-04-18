62f62a89c83bfa1a074d96beb18d93e23c2671b0
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-18 11:48:05 +0530

    first commit

---
+++ build/CMakeFiles/MyTextEditor.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/mathewsvinoy/820831ee-706c-42e4-8979-f6876adb34d6/dev/Text_Editor/src/main.cpp" "CMakeFiles/MyTextEditor.dir/src/main.cpp.o" "gcc" "CMakeFiles/MyTextEditor.dir/src/main.cpp.o.d"
+  "/media/mathewsvinoy/820831ee-706c-42e4-8979-f6876adb34d6/dev/Text_Editor/src/window.cpp" "CMakeFiles/MyTextEditor.dir/src/window.cpp.o" "gcc" "CMakeFiles/MyTextEditor.dir/src/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyTextEditor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTextEditor.dir/src/main.cpp.o"
+  "CMakeFiles/MyTextEditor.dir/src/main.cpp.o.d"
+  "CMakeFiles/MyTextEditor.dir/src/window.cpp.o"
+  "CMakeFiles/MyTextEditor.dir/src/window.cpp.o.d"
+  "MyTextEditor"
+  "MyTextEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyTextEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
